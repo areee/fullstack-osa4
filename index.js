dba4f32825991260d23935ad0b34e8aaa004a16d
areee <10089872+areee@users.noreply.github.com>
2020-08-15 20:25:06 +0300

    4.2: use blog model

--- index.js
+++ index.js
@@ -3,19 +3,11 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const Blog = require('./models/blog')
 
 // [alku] tässä 4.2:n lopulliset requiret
 const config = require('./utils/config')
 // [loppu]
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
