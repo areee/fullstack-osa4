411b5d85e9a2384dee7375f5c73e721e52d3c091
areee <10089872+areee@users.noreply.github.com>
2020-08-16 13:00:38 +0300

    4.2 ready (refactored = use several modules)

--- index.js
+++ index.js
@@ -1,34 +1,9 @@
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const Blog = require('./models/blog')
-
-// [alku] tässä 4.2:n lopulliset requiret
 const config = require('./utils/config')
-// [loppu]
 
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+const server = http.createServer(app)
 
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
 })
