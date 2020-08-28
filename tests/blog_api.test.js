646e54e83adde04e5fa7b61dc98c2948a15f9c6c
Arttu Ylhävuori <10089872+areee@users.noreply.github.com>
2020-08-28 16:09:19 +0300

    4.8: finished the test (but refactoring still needed)

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,12 +1,40 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
 
-test('there are eight blogs', async () => {
-  const response = await api.get('/api/blogs')
-  expect(response.body).toHaveLength(8)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: 'Yle',
+    author: 'Yleisradio',
+    url: 'https://yle.fi/',
+    likes: 5,
+  },
+  {
+    title: 'Chromium Blog',
+    author: 'Google',
+    url: 'https://blog.chromium.org/',
+    likes: 16,
+  },
+]
+
+describe('when there is initially some blogs saved', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+  })
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(initialBlogs.length)
+  })
 })
 
 afterAll(() => {
