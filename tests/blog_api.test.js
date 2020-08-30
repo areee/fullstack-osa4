5f25bec6244e791c39b98501a231b0ed93b20819
areee <10089872+areee@users.noreply.github.com>
2020-08-30 16:10:02 +0300

    4.12: nearly ready (added a test with a blog without required 'title' and 'url' fields)

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -58,6 +58,16 @@ describe('when there is initially some blogs saved', () => {
     expect(blogsAtEnd.length).toBe(helper.initialBlogs.length + 1)
     expect(blogsAtEnd[helper.initialBlogs.length].likes).toBe(0)
   })
+
+  test('a blog without title and url is not added', async () => {
+    const newBlog = {
+      author: 'Arttu',
+      likes: 55,
+    }
+    await api.post('/api/blogs').send(newBlog).expect(400)
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
 })
 
 afterAll(() => {
